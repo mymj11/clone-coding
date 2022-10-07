9486e326185b6d6b0496404281f538e4b756aa5c
김민지 <pass8208@gmail.com>
2022-10-07 15:10:33 +0900

    feat:leftpane2

--- react/twitter-clone/src/App.js
+++ react/twitter-clone/src/App.js
@@ -1,21 +1,25 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { LeftPane } from './components';
-import './App.css';
+import './App.scss';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="App">
+    <div className="app">
       <LeftPane />
-      <Switch>
+      <Routes>
         <Route path="/" exact>Home</Route>
+        {/* 정확한 경로를 표시할 때 exact를 쓴다? */}
         <Route path="/explore">Explore</Route>
         <Route path="/notification">Notification</Route>
         <Route path="/messages">Messages</Route>
         <Route path="/bookmarks">Bookmarks</Route>
         <Route path="/lists">Lists</Route>
         <Route path="/profile">Profile</Route>
-      </Switch>
+        {/* localhost:3000/explore과 같이 슬래시를 추가하여 경로를 적으면 해당 페이지가 뜬다. */}
+      </Routes>
+      {/* 이제는 Switch를 쓰지 않고 Routes를 쓴다. */}
+      <div>Messages</div>
       <div>Right Pane</div>
     </div>
     </BrowserRouter>
--- react/twitter-clone/src/components/leftPane/LeftPane.jsx
+++ react/twitter-clone/src/components/leftPane/LeftPane.jsx
@@ -5,7 +5,10 @@ const LeftPane = () => {
     return(
         <div className="left-pane">
             <div className="container">
+                {/* container div의 위치를 고정할 것이다. 
+                container 내부의 다른 섹션을 스크롤하는 동안 화면에 고정될 것이다.*/}
                 <header>Twitter icon here</header>
+                {/* header에는 트위터 아이콘을 넣을 것. */}
                 <nav>
                     <NavLink to="/">
                         Home
@@ -29,16 +32,29 @@ const LeftPane = () => {
                         Profile
                     </NavLink>
                 </nav>
+                {/* navlink를 사용하여 react twitter 앱 탐색하기. */}
+                {/* 내부에 웹 애플리케이션을 구축할 때 접근성을 고려해야 한다.
+                react-router-dom의 탐색 링크 구성 요소 사용.
+                다른 구성 요소를 탐색하는데 사용할 것이므로 navlink를 가져오자. 
+                탐색 링크를 클릭하면 페이지를 다시 로드하지 않고 구성요소로 이동할 수 있다.*/}
 
                 <button className="tweet">Tweet</button>
 
+                {/* LeftPane 구성요소에 바닥글 추가. */}
                 <footer>
                     <button className="account">
+                    {/* 계정 버튼.
+                    계정 버튼 내부에 이미지와 사용자 이름이 포함.*/}
                         <div className="photo">
                             <img 
-                                alt="User"
+                                alt="user"
                                 src="./img/blank-profile-picture.webp"
                             />
+                        </div>
+                        {/* 하나의 div안에 두 개의 열이 있는데 1번 열에는 사진이 있고 2번 열에는 두 개의 행이 포함되게 하자.*/}
+                        <div>
+                            <div className="name">name</div>
+                            <div className="username">@username</div>
                         </div>
                     </button>
                 </footer>
