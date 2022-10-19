e519621ed6ded441d91cac89b32a69a48abf20d9
mymj11 <pass8208@gmail.com>
2022-10-20 06:34:50 +0900

    feat:chat room

--- Node.js/zoom/src/public/js/app.js
+++ Node.js/zoom/src/public/js/app.js
@@ -1,169 +1,235 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-//브라우저에서는, 여기 이 백엔에 연결했고?
-//프엔에서 백엔으로 연결을 만들어주고
-const socket = new WebSocket(`ws://${window.location.host}`);
-//새로고침할 때 이게 작동한다. 그러면 모든 addEventListener가 설정이 될 것이다.
-//이제 서버로 접속할 수 있고, 폰으로도 가능하다.
-//WebSocket은 브라우저와 서버 사이의 연결이다.
-//프엔도 socket을 가지고 있다.여기 프엔에서 백엔으로 메시지를 보낼 수 있다. 그리고 백엔으로부터 메시지를 받을 수 있다.
-//app.js의 socket은 서버로의 연결을 뜻한다.
+const socket = io();
+//io는 자동적으로 back-end socket.io와 연결해주는 function
+//io function은 알아서 socket.io를 실행하고 있는 서버를 찾을 거다.
 
-//프엔에서도 socket을 만들었다.
-//그리고 3개의 event에 대해 listen하고 있다.
-//하나는 open이고, connection이 open되면 Connected to Server ✔를 볼 수 있다.
-//또 하나는 메시지를 받을 때마다 내용을 출력하는 message다.
-//나머지 하나는 close다.
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+//home.pug의 welcome div에서 form을 가져오자.
+const room = document.getElementById("room");
 
-function makeMessage(type, payload){
-    const msg = {type, payload};
-    //object 만들기
-    //메시지를 만들어주자.
-    return JSON.stringify(msg);
-    //이 object를 string으로 만들었다.
-    //그리고 메시지를 string으로 바꿔서 return하자.
+room.hidden = true;
+//들어가기 전에 채팅방이 숨겨진다.
+
+let roomName;
+
+//메시지를 추가해주는 function
+//message를 받을 거고
+function addMessage(message){
+    const ul = room.querySelector("ul")
+    const li = document.createElement("li")
+    //li를 만들어주자.
+    li.innerText = message;
+    //누가 입장했는지는 모르지만 누군가 입장했다는 것을 알 수 있다.
+    ul.appendChild(li);
 }
-//이 함수는 type과 payload를 받을 거다.
-//이제 메시지를 전송하고 싶으면, makeMessage function을 불러주면 된다.
-//화면 input창에 닉네임을 입력하고 버튼을 누르면 
-//백엔드가 메시지를 다시 전송해줘서 화면에 string 형태로 보여준다.
 
-//익명함수를 쓰지 싫다면,
-// function handleOpen(){
+function showRoom(){
+   welcome.hidden = true;
+   room.hidden = false;
+   const h3 = room.querySelector("h3");
+   h3.innerText = `Room ${roomName}`;
+}
+//안에서는 form#welcome을 가지고 와야한다.
+
+// function backendDone(msg){
+//     console.log(`The backend says: `, msg);
+// }
+//백엔에서 이 function에 argument를 보낼 수 있다.
+//백엔에서 이 function을 호출할 때 "hello from the backend"를 같이 보내보자.
+
+// function backendDone(){
+//     console.log("backend done");
+// }
+//프엔에서 실행된 코드는 백엔드가 시킨거다. //백엔드에서 프엔의 이 코드를 실행시켰다.
+//vs 터미널에는 input창에 입력한 방이름이 뜨고 콘솔창에는 backend done이 뜬다.
+//백엔에서 실행시키는 코드를 프엔에서 만들 수 있다.
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
+    //roomName은 처음에는 비어있다.
+    input.value = ""
+    //input.value는 방 이름.
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+    addMessage("someone joined!");
+});
+//welcome event가 발생하면 addMessage() function을 호출한다. 
+
+// socket.on("welcome", () => {
+//     const ul = room.querySelector("ul")
+//     const li = document.createElement("li")
+//     li.innerText = "Someone joined!"
+//     ul.appendChild(li);
+// });
+//여기에 계속 메시지를 추가해 주는 것보다 메시지를 추가해주는 함수를 만드는 것이 더 좋다.
+
+    //방 이름을 가지고 입장을 하면 showRoom이라는 function을 실행시켜 줄 것이다.
+    //function showRoom을 서버에 보내고 서버에서 실행도 시켜야 한다.
+    //function backendDone을 백엔드로 보내고 있다.
+    //백엔드는 두 개의 argument를 받는다.(input.value, backendDone)
+
+    // socket.emit("enter_room", {payload: input.value}, () => {
+    //     console.log("server is done!");
+    //});
+    //enter_room이라는 event를 emit해준다. 항상 메시지를 보낼 필요없다. (room 메시지 전송)
+    //어떤 event든지 전송할 수 있다.
+    //2번째 argument에는 보내고 싶은 payload가 들어간다.
+    //emit을 하면 argument를 보낼 수 있다. argument는 object가 될 수 있다.
+    //자바스크립트 object를 전송할 수 있다. websocket처럼 object를 string으로 바꿔줄 필요가 없다.
+    //sockek.emit의 3번째 argument로 function을 넣어주자. 서버에서 호출하는 function이 들어간다.
+    //백엔드에서 끝났다는 사실을 알리기 위해 function을 넣고 싶으면 그 function이 가장 마지막 argument가 되어야 한다.
+
+//event를 받고 event.preventDefault()를 쓰고 Form 안에서 input을 가져오자.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// websocket code
+// const messageList = document.querySelector("ul");
+// const nickForm = document.querySelector("#nick");
+// const messageForm = document.querySelector("#message");
+//     //프엔에서 백엔으로 연결을 만들어주고
+// const socket = new WebSocket(`ws://${window.location.host}`);
+//     //WebSocket은 브라우저와 서버 사이의 연결이다.
+//     //프엔도 socket을 가지고 있다.여기 프엔에서 백엔으로 메시지를 보낼 수 있다. 그리고 백엔으로부터 메시지를 받을 수 있다.
+//     //app.js의 socket은 서버로의 연결을 뜻한다.
+
+// function makeMessage(type, payload){
+//     const msg = {type, payload};
+//         //object 만들기 //메시지를 만들어주자.
+//     return JSON.stringify(msg);
+//         //이 object를 string으로 만들었다. //그리고 메시지를 string으로 바꿔서 return하자.
+// }
+//     //이 함수는 type과 payload를 받을 거다.
+//     //이제 메시지를 전송하고 싶으면, makeMessage function을 불러주면 된다.
+
+//     //익명함수를 쓰지 싫다면,
+//     // function handleOpen(){
+//     //     console.log("Connected to Server ✔");
+//     // }
+//     // socket.addEventListener("open", handleOpen);
+
+// socket.addEventListener("open", () => {
 //     console.log("Connected to Server ✔");
+// });
+//     //connection이 open일 때 사용하는 listener를 등록했고
+//     //server.js에서 메시지를 보냈으니 app.js에서 메시지를 받아야 한다.
+//     //서버랑 연결이 되면 방금 받은 메시지가 나타난다. 이게 바로 MessageEvent다.
+
+//     //근데 만약 프엔에서 많은 것을 보내고 싶어서 object를 보내고 싶다면
+//     //백엔에서 JSON.stringity를 이용해서 message를 만들어줘야 한다.
+//     //그리고 프엔에서 JSON.parse를 해주는 새로운 함수를 만들어야 한다.
+
+//     //예를 들어, 여기서 유저가 메시지를 전송하면
+//     //socket이 메시지를 받을 때 실행되는 코드 //하지만 나를 제외한 모든 사람들에게만 메시지를 보내고 싶다.
+// socket.addEventListener("message", (message) => {
+//     const li = document.createElement("li");
+//         //메시지를 스크린에 보여주는 function을 만들어 보자. //새로운 메시지를 받으면, 먼저 새로운 li를 만들 것이다.
+//     li.innerText = message.data;
+//         //다음에는 messsage.data를 li 안에 넣어 주자.
+//     messageList.append(li);
+//         //그 다음, li를 messageList 안에 넣어 주자. //이렇게 해주면 메시지를 화면에 보여줄 수 있다.
+//         //form을 만들어서 nickname을 정할 수 있도록 해주자.(html파일에서-home.pug)
+
+//         //console.log("Just got this: ", message.data, "from the server");
+//     console.log("New message: ", message.data);
+// });
+//     //서버로부터? 메시지를 받았을 때 사용하는 listener를 등록했고
+//     //2단계, 이번에는 socket에 message 이벤트를 추가해보자. 이렇게 해서 메시지를 받아보자.
+//     //서버로부터 메시지를 받을 때 발생.
+
+// socket.addEventListener("close", () => {
+//     console.log("Disconnected from Server ❌");
+// });
+//     //서버가 오프라인이 됐을 때 사용하는 listener도 등록했다.
+//     //socket에 close 이벤트를 추가해보자. //서버로부터 연결이 끊어졌을 때 발생.
+
+//     //메시지를? 백엔드로 보낸다.
+//     //chat으로 보내는 메시지.
+// function handleSubmit(event){
+//     event.preventDefault();
+//     const input = messageForm.querySelector("input");
+//         //여기서 input을 가지고 올 건데 const input을 쓰고 form에서 input을 찾아오자.
+//     socket.send(makeMessage("new_message", input.value));
+//         //type은 "new_message", payload는 input.value
+//     const li = document.createElement("li");
+//     li.innerText = `You: ${input.value}`;
+//     messageList.append(li);
+//     input.value = "";
+//         //input창에 메시지를 입력하고 send 버튼을 누르면 input창이 비워진다.
+//         //그리고 모든 메시지는 백엔으로 가고 있다. vs 터미널에서 확인할 수 있다.
+//         //메시지를 보낸 후, 값을 비워주고 있다. 그리고 그 메시지는 서버로 간다.
+
+//         // console.log(input.value);
+//         //여기서는 메시지의 내용이 될 input.value를 출력하자.
 // }
-// socket.addEventListener("open", handleOpen);
+//     //이게 event를 준다는 것을 알고 있다.
+//     //input창에 hello를 하고 send 버튼을 눌렀더니(enter를 쳐도된다.) 콘솔창에 hello라고 뜬다.
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ✔");
-});
-//connection이 open일 때 사용하는 listener를 등록했고
-//server.js에서 메시지를 보냈으니 app.js에서 메시지를 받아야 한다.
-//일단 open이라는 event를 써보자. socket이 open이 되었다면, 서버에 연결되었다고 로그를 출력하자.
-//추가한 체크 이모지는 socket이 connection을 open했을 때 발생할 것이다.
-//서버랑 연결이 되면 방금 받은 메시지가 나타난다. 이게 바로 MessageEvent다.
-//서버와 연결되었을 때 발생.
+//     //위와 아래의 코드를 보면 서로 다른 form에서 전송되고 있다. 왜냐하면 form이 두 개니까.
+//     //자바스크립트 objectr를 백엔드로 보내면 안 된다. string을 보내야 한다. 연결하고 싶은 서버가 자바스크립트 서버가 아닐 수 있으니까.
 
+//     //닉네임을 설정해줬을 때 //닉네임을 변경하고 싶을 때 백엔드로 보내고 있다.
+// function handleNickSubmit(event){
+//     event.preventDefault();
+//     const input = nickForm.querySelector("input");
+//         // socket.send(input.value);
+//     socket.send(makeMessage("nickname", input.value));
+//         //여기서 우리는 string을 보내주고 있다.
+//         //백엔드에서 이 string을 자바스크립트 object로 바꿔야 한다.({"type":"new_message","payload":"gg"})
+//     input.value = "";
+//         //닉네임값을 비워주자.
 
-//예를 들어, 여기서 유저가 메시지를 전송하면
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    //메시지를 스크린에 보여주는 function을 만들어 보자.
-    //새로운 메시지를 받으면, 먼저 새로운 li를 만들 것이다.
-    li.innerText = message.data;
-    //다음에는 messsage.data를 li 안에 넣어 주자.
-    messageList.append(li);
-    //그 다음, li를 messageList 안에 넣어 주자.
-    //이렇게 해주면 메시지를 화면에 보여줄 수 있다.
-    //firefox에서 메시지를 보냈더니 firefox 화면과 brave 화면에 다 메시지가 보인다.
-    //form을 만들어서 nickname을 정할 수 있도록 해주자.(html파일에서-home.pug)
-
-    //console.log("Just got this: ", message.data, "from the server");
-    console.log("New message: ", message.data);
-});
-//서버로부터? 메시지를 받았을 때 사용하는 listener를 등록했고
-//2단계, 이번에는 socket에 message 이벤트를 추가해보자. 이렇게 해서 메시지를 받아보자.
-//그리고 '이 메시지를 서버로부터 받아왔습니다.'라고 출력해보자.
-//개발자 도구 콘솔에서 Just got this: hello!!! from the server라는 메시지가 뜬다.
-//연결이 alive 상태다. 터미널을 보면 server를 보면 브라우저와 연결돼었다는 것을 알 수 있다.
-//서버로부터 메시지를 받을 때 발생.
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server ❌");
-});
-//서버가 오프라인이 됐을 때 사용하는 listener도 등록했다.
-//socket에 close 이벤트를 추가해보자.
-//서버와 연결이 끊어졌다고 써보자.
-//서버로부터 연결이 끊어졌을 때 발생.
-
-//메시지를? 백엔드로 보낸다.
-//chat으로 보내는 메시지.
-function handleSubmit(event){
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    //여기서 input을 가지고 올 건데 const input을 쓰고 form에서 input을 찾아오자.
-    socket.send(makeMessage("new_message", input.value));
-    //type은 "new_message", payload는 input.value
-
-    //socket.send(input.value);
-    //2번째 단계는 input.value를 출력하는 대신 socket.send()를 사용하자.
-    //프엔의 form에서 백엔으로 무언가를 보내고 있다.
-    input.value = "";
-    //input창에 메시지를 입력하고 send 버튼을 누르면 input창이 비워진다.
-    //그리고 모든 메시지는 백엔으로 가고 있다. vs 터미널에서 확인할 수 있다.
-
-    // console.log(input.value);
-    //여기서는 메시지의 내용이 될 input.value를 출력하자.
-}
-//이게 event를 준다는 것을 알고 있다.
-//input창에 hello를 하고 send 버튼을 눌렀더니(enter를 쳐도된다.) 콘솔창에 hello라고 뜬다.
-
-//위와 아래의 코드를 보면
-//서로 다른 form에서 전송되고 있다. 왜냐하면 form이 두 개니까.
-//자바스크립트 object를 백엔드로 보내면 안 된다. 연결하고 싶은 서버가 자바스크립트 서버가 아닐 수 있으니까.
-//그래서 string을 보내야 한다. 백엔드에 있는 모든 서버에 그 string을 가지고 뭘 할지 정하자.
-
-//닉네임을 설정해줬을 때
-//닉네임을 변경하고 싶을 때 백엔드로 보내고 있다.
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    // socket.send(input.value);
-    socket.send(makeMessage("nickname", input.value));
-    //여기서 우리는 string을 보내주고 있다.
-    //백엔드에서 이 string을 자바스크립트 object로 바꿔야 한다.({"type":"new_message","payload":"gg"})
-
-    // socket.send({
-    //     type:"nickname",
-    //     payload:input.value,
-    //     //input값을 넣자. 즉, user가 입력한 값.
-    // });
-    //내가 저장한 닉네임이 이 메시지로 전송된다.
-    //sever.js에서 내가 받고 싶은 type과 playload 형태로 전송될 것이다.
-    //지금 text를 전송하는 게 아니라, JSON object 전체를 전송하고 있다.
-    //[object, object]
-}
-//input을 가져오는 코드와 데이터를 보내는 코드랑 같다.
-//그럼 이제 백엔드로 메시지를 전송할 때마다 우리는 string을 전송해 줄거다.
-//하지만 string을 보내기 전에, object를 만들고 그 object를 string으로 만들었다.
-
-
-messageForm.addEventListener("submit", handleSubmit);
-//event listener를 messageForm에 추가해 줄 거다.
-nickForm.addEventListener("submit", handleNickSubmit);
-
-
-
-// setTimeout(() => {
-//     socket.send("hello from the browser!");
-// }, 10000);
-//이제 프엔에서 백엔으로 메시지를 보내보자.
-//즉시 실행되길 원하지 않기 때문에 timeout을 사용하자.
-//메시지를 보내기까지 10초를 기다리자.
-//socket.을 쓰고 send라는 메서드를 사용하자.
-//이제 server.js에 socket.on()을 쓰고 message 이벤트를 등록할 거다.
-//그럼 이제 브라우저에서 보낸 hello를, server.js로 가서 백엔에서 받을 수 있는지 확인해보자.
-//터미널에서 브라우저가 연결되었다는 메시지가 뜨고 10초 후 hello from the browser!를 확인할 수 있다.
-
-
-// btn.addEventListener("click", fn);
-//click이라는 이벤트를 만들고 callback을 적으면 된다.
-//click을 했을 때 작동할 function을 넣으면 된다.
-//이게 websocket이랑 비슷하다.
-//webSocket도 event가 있고, event가 발동될 때 사용할 function을 만들면 된다.
-//click 대신 submit도 될 수 있다.
-
-// function fn(event){
-
+//         // socket.send({
+//         //     type:"nickname",
+//         //     payload:input.value,
+//         //     //input값을 넣자. 즉, user가 입력한 값.
+//         // });
+//         //내가 저장한 닉네임이 이 메시지로 전송된다.
+//         //sever.js에서 내가 받고 싶은 type과 playload 형태로 전송될 것이다.
+//         //지금 text를 전송하는 게 아니라, JSON object 전체를 전송하고 있다.
+//         //[object, object]
 // }
-//event가 안에 있는 것을 안다.
-//JS는 event의 정보랑 같이 function을 호출할거다.
-//어떤 일이 발생했는 지에 관한 정보도 말이다. 브라우저도 마찬가지다.
+//     //input을 가져오는 코드와 데이터를 보내는 코드랑 같다.
+//     //그럼 이제 백엔드로 메시지를 전송할 때마다 우리는 string을 전송해 줄거다.
+//     //하지만 string을 보내기 전에, object를 만들고 그 object를 string으로 만들었다.
 
-//form.addEventListener("submit", fn);
-//프엔에서 form이 있다고 하자.
-//submit event를 listen한다고 하자.
-//JS는 function을 가지고 function을 호출하고, 정보를 줄 거다.
+// messageForm.addEventListener("submit", handleSubmit);
+//     //event listener를 messageForm에 추가해 줄 거다.
+// nickForm.addEventListener("submit", handleNickSubmit);
+
 
 
--- Node.js/zoom/src/server.js
+++ Node.js/zoom/src/server.js
@@ -1,149 +1,171 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
+// import WebSocket from "ws";
 import express from "express";
+import { emit } from "process";
 
 const app = express();
 //app 만들기
 
 app.set('view engine', "pug");
-//Pug로 view engine을 설정하고 
+//Pug로 view engine을 설정하고
+
 app.set("views", __dirname + "/views");
 //Express에 template이 어디 있는지 지정해주고
 //여기에 view engine을 Pug로 설정하고, views 디렉토리가 설정되고
 //나중에 pug 페이지들을 렌더하기 위해 pug 설정을 해줘야 한다.
-//views 디렉토리를 입력
 
 app.use("/public", express.static(__dirname + "/public"));
 //public url을 생성해서 유저에게 파일을 공유해주고
-//유저가 /public으로 가게되면 __dirname + "/public" 폴더를 보여주게 할 거다.
-//public 폴더를 유저에게 공개해준다.
-//유저는 /public으로 이동할 시 public 폴더 내용을 볼 수 있다.
-//localhost:3000/public/js/app.js하면 아직 컨텐츠는 없지만 "cannot get/" 메시지는 없다.
-//그 말인즉, /public/js에 있는 app.js에 hello라고 입력하게 되면 hello라고 뜬다.
-
+//유저가 /public으로 가게되면 () 폴더를 보여주게 할 거다. public 폴더를 유저에게 공개해준다.
 
 //우리가 사용할 유일한 route 만들기
-//home으로 기면 request, response를 받고 res.render를 할 거다.
-//우리가 만든 home을 렌더하는 것.
+//home으로 가면 request, response를 받고 res.render를 할 거다. //우리가 만든 home을 렌더하는 것.
 app.get("/", (req, res) => res.render("home"));
-//home.pug를 render해주는 route handler를 만들었다.
-//우리 홈페이지로 이동 시 사용될 템플릿을 렌더해주는 것이다.
+//home.pug를 render해주는 route handler를 만들었다. //우리 홈페이지로 이동 시 사용될 템플릿을 렌더해주는 것이다.
 //유저가 홈(/)으로 GET request를 보내면 template으로 반응한다.
-//http가 어떻게 생겼냐면
-//url을 선언하고, 유저가 url로 가면, req와 res를 받고, response를 보낸다.
+//http가 어떻게 생겼냐면 url을 선언하고, 유저가 url로 가면, req와 res를 받고, response를 보낸다.
+
 app.get("/*", (req, res) => res.redirect("/"));
 //유저가 어떤 페이지로 GET request를 보내도 redirect로 반응하도록 해뒀다.
 //app.get을 입력하고 여기서 유저가 어떤 url로 이동하던지 홈으로 돌려보내면 된다. 
 //왜냐하면 다른 url을 사용하지 않을 거니까. 다른 url은 전혀 사용하지 않고 홈만 사용할 것이다.
 //주소창에 아무거나 입력해도(localhost:3000/dfdfdfd) 홈(localhost:3000)으로 돌려보내지게 된다.
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-//express는 http를 다루지만, 이젠 ws를 다룰거다. 여기에 2개를 합칠거다.
-//express로 같은 서버에 ws 기능을 설치할 거다.
-
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 //express.js를 이용해서 http 서버를 만들었다. 
-//여기서 서버를 만들건데, crateServer를 하려면 requestListener 경로가 있어야 한다.
-//이게 우리의 application이 되는 것이다.
-//express application으로부터 서버를 만들어보자.
-//서버에 접근할 수 있게 되었기 때문에 준비는 다 끝났다.
-//즉, 이제 이 서버에서 websocket을 만들 수 있다는 것이다.
+//여기서 서버를 만들건데, crateServer를 하려면 requestListener 경로가 있어야 한다. 이게 우리의 application이 되는 것이다.
+//express application으로부터 서버를 만들어보자. 이 서버에서 websocket을 만들 수 있다는 것이다.
 //app.listen 하기 전인데, 아직 서버에 access(접근)하지 못했었는데 이제 서버에 접근할 수 있다.
-//이제 ws를 import해보자.
 
-const wss = new WebSocket.Server({server});
-//이제 새로운 websocket 서버를 만들어 볼 거다.
-//그리고 여기서 server를 전달해줄 수 있다.
-//() 안에 server를 굳이 넣지 않아도 된다. 필수는 아니다.
-//서버를 전달(pass)해줄 거다.
-//http 서버 위에 webSocket 서버를 만들었다.
+const wsServer = SocketIO(httpServer);
+//const io = SocketIO(server);
+//server를 보내주는 io 서버를 만들자.(wsServer라고해도 됨)
 
-const sockets = [];
-//array
-//fake database를 만들어 줄 거다.
-//누군가 우리 서버에 연결하면, 그 connection을 이 sockets에 넣을 거다.
-//socket이 2개가 생긴다. //const sockets = [1,2];
-//이렇게 하면 받은 메시지를 다른 모든 socket에 전달해 줄 수 있다.
-//brave가 연결되면 brave의 socket을 가져와서 [brave] 여기에 넣어 줄거다.
-//그리고 firefox가 연결되면 그 socket을 넣어 줄거다. //[brave, firefox]
-//이제 아래 wss.on에서 brave에서 메시지를 받으면,
-//받은 메시지를 sockets에 있는 모든 곳에 전달해 줄 수 있다.
-//firefox에서 메시지를 받았을 때, 나는 firefox와 brave 모두에게 메시지를 전달해주는 거다.
-//추후에 이 function은 바꿀 거다. 나 스스로한테 메시지를 보낼 필요가 없어서.
-
-
-// function handleConnection(socket){
-//     console.log(socket);
-// }
-// wss.on("connection", handleConnection);
-
-
-//만약 익명함수를 쓰고 싶지 않다면, 따로 빼서 onSocketClose function을 만들면 된다.
-//function onSocketMessage는 message를 받을 것이다.
-//socket 코드는 짧아지고, 대신 분리된 function이 생긴다.
-// function onSocketClose(){
-//     console.log("Disconnected from Browser ❌");
-// }
-
-// function onSocketMessage(message){
-//     console.log(message.toString('UTF8'));
-// }
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    //누군가 우리 서버에 연결하면, 그 connection을 이 socket에 넣을 거다.
-    //firefox가 연결될 때, firefox를 위 array에 넣어준다는 것.
-    //그리고 brave가 연결될 때는 brave를 array에 넣어 줄거다. //그럼 socket 2개가 생기는 것.
-    console.log("Connected to Browser ✔");
-    //브라우저가 연결되면, 무언가를 console.log하고
-
-    //만약 익명함수를 쓰고 싶지 않다면, 따로 빼서 onSocketClose function을 만들면 된다.
-    //socket.on("close", onSocketClose);
-
-    socket.on("close", () => {
-        console.log("Disconnected from Browser ❌");
+wsServer.on("connection", (socket) => {
+    socket.onAny((event) => {
+        console.log(`Socket Event:${event}`);
     });
-    //브라우저가 꺼졌을 때를 위한 listener를 등록했다.
-    //() => {console.log("Disconnected from Browser ❌");}는 이름없는 function이다.(익명함수)
-    //socket이 connection을 종료시키면 예를 들어, 브라우저의 탭을 닫거나, 컴퓨터가 잠자기 모드에 들어가면
-
-    //socket.on("message", onSocketMessage);
-
-    socket.on("message", (message) => {
-        const parsed = JSON.parse(message.toString('UTF8'));
-        //JSON.stringity는 JavaScript object를 string으로 바꿔준다.
-        //그리고 JSON.parse는 string을 JavaScript object로 바꿔준다.
-        //console.log(parsed, message.toString('UTF8'));
-        //{ type: 'new_message', payload: 'aaaa' }는 자바스크립트 object
-        //{"type":"new_message","payload":"aaaa"}는 string
-        if(parsed.type === "new_message"){
-        sockets.forEach((aSocket) => aSocket.send(parsed.payload));
-        } else if(parsed.type === "nickname"){
-            console.log(parsed.payload);
-        }
-        //새 메시지를 확인하고 모두에게 보낼 것이다. 나는 parsed.payload를 보낼 것이다.
-        //메시지를 입력하면 화면에 메시지가 보인다. 하지만 닉네임은 입력해도 화면에 보이지 않는다.
-        //이는 이제 서버에서 어떤 메시지를 받고 있는지 구분하고 있다는 것을 말한다.
-        //이제 메시지는 type(메시지 종류)과 payload(메시지에 담겨있는 중요한 정보) 두 가지를 가지고 있다.
-
-        //socket.send(message.toString('utf8')); //socket이 메시지를 보냈다.
-        //기본적으로 user로부터 메시지를 받아서 다시 돌려주고 있다. 즉, 혼자 대화하는 것.
-        //백엔으로부터 엄청 빠르게 메시지를 받은 것을 개발자 도구 콘솔창에서 확인할 수 있다.
-        //방금 내가 적은 것을 받은 거다. hi라고 하면 새 메시지로 hi를 받는다.
-        //메시지를 보낸 socket에 다시 메시지를 보내는 대신,
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        //roomName으로 방에 참가하자.
+        done();
+        //프엔의 function showRoom을 서버에서 실행시키자.
+        socket.to(roomName).emit("welcome");
+        //참가했다는 것을 나를 제외한 방에 있는 모든 사람들에게 알려주자.
+        //event를 emit해주자. event의 이름은 마음대로.
+        //welcome event를 나를 제외한 roomName에 있는 모든 사람들에게 emit한 것이다.
     });
 });
-//여기 aSocket은 socket과 다른 것. 
-//각 브라우저를 aSocket으로 표시하고 메시지를 보낸다는 의미.
-//이제 연결된 모든 socket들에 접근할 수 있다.
-//메시지를 보낼 때, 모두에게 보내고 있다.
-//내 메시지가 백엔에 전달이 되면, 백엔드는 다른 모두에게 전송을 해주게 된다.
-//지금 2가지 type의 메시지가 있다.
-//지금부터는 그냥 text를 보내는 대신 JSON을 보낼거다.
-//여기 이 메시지는 string이다. 
-//이 string을 자바스크립트 object로 만들어야 type을 확인할 수 있다.(JSON.parse() 사용)
+//방에 참가하면 done() function을 호출한다.
+//done()은 프엔에 있는 showRoom()을 실행한다.
+//그러면 event를 방금 참가한 방 안에 있는 모든 사람들에게 emit해주는 거다.
+//프엔에서 이 event를 반응하도록 만들자.(socket.on())
 
-server.listen(3000, handleListen);
-//이제 할 것은, server.listen 할 수 있다. port는 아무거나.
-//이제 handListen을 사용할 수 있다.
 
+        // setTimeout(() => {
+        //     done("hello from the backend");
+        // }, 15000);
+    //15초 후에 백엔드는 done function을 argument와 같이 call할거다.
+    //백엔드는 두 개의 argument를 받는다.(input.value, backendDone)
+    //한 개는 방 이름이고, 다른 하나는 우리가 call할 function이다.(끝났을 때 call하니까. 콜백은 서버로부터 실행되는 함수)
+    //done function을 call했을 때 백엔드가 이 코드를 실행시키는 것이 아니다. 프엔에서의 function을 실행시킨 것이다.
+
+    // socket.on("enter_room", (msg, done) => {
+    //     console.log(msg);
+    //     setTimeout(() => {
+    //         done();
+    //     }, 10000);
+    //});
+
+//받는 socket이 있다. //백엔드에서 connection을 받을 준비 끝.
+//msg를 받을 수 있다. 여기서 msg는 프엔에서 보내는 argument 중 JSON object이다.
+//2번째로는 프엔에서 보내는 done이라는 이름을 가진 function을 받는다.
+//서버에서 done이라는 function을 호출하는 것, 서버가 이 function을 실행시키면 이 funtion은 프엔에서 실행될 것이다.
+//서버는 백엔에서 function을 호출하지만, function은 프엔에서 실행되는 것이다.
+//프엔에서 object를 보내고 백엔에서 console.log하면 object를 받을 수 있다.
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
+
+
+
+
+
+
+//websocket code (vs socket.io code)
+
+// const wss = new WebSocket.Server({server});
+//     //서버를 전달(pass)해줄 거다.
+//     //새로운 websocket을 만들 때 HTTP를 위에 쌓아올리면서 만들었다.
+
+// const sockets = []; //array
+//     //누군가 우리 서버에 연결하면, 그 connection을 이 sockets에 넣을 거다.
+//     //brave와 firefox가 각각 연결되면 그 socket을 넣어 줄거다. //[brave, firefox] //socket이 2개가 생긴다.
+//     //이제 아래 wss.on에서 brave에서 메시지를 받으면, 받은 메시지를 sockets에 있는 모든 곳에 전달해 줄 수 있다.
+//     //firefox에서 메시지를 받았을 때, 나는 firefox와 brave 모두에게 메시지를 전달해주는 거다.
+
+//     // function handleConnection(socket){
+//     //     console.log(socket);
+//     // }
+//     // wss.on("connection", handleConnection);
+
+//     //만약 익명함수를 쓰고 싶지 않다면, 따로 빼서 onSocketClose function을 만들면 된다.
+//     //function onSocketMessage는 message를 받을 것이다.
+//     // function onSocketClose(){
+//     //     console.log("Disconnected from Browser ❌");
+//     // }
+
+//     // function onSocketMessage(message){
+//     //     console.log(message.toString('UTF8'));
+//     // }
+
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//         //socket이 연결될 때 //누군가 우리 서버에 연결하면, 그 connection을 이 socket에 넣을 거다.
+//         //firefox와 brave가 연결될 때는 각각 array에 넣어 줄거다. //그럼 socket 2개가 생기는 것.
+//     socket["nickname"] = "Anon";
+//         //각 socket에 anon이라는 nickname을 정해줬다. //닉네임이 없는 경우다. 익명이라는 닉네임이니까.
+//         //익명이기 때문에 brave와 firefox에서 모두 실행되었다. 
+//     console.log("Connected to Browser ✔");
+//         //브라우저가 연결되면, 무언가를 console.log하고
+
+//     socket.on("close", () => {
+//         console.log("Disconnected from Browser ❌");
+//     });
+//         //브라우저가 꺼졌을 때를 위한 listener를 등록했다. close를 listen 해준다.
+//         //() => {console.log("");}는 이름없는 function이다.(익명함수)
+
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg.toString('UTF8'));
+//             //브라우저에서 보낸 메시지가 여기로 도착한다. 그 메시지를 처리하고, type을 확인한다.
+//             //JSON.stringity는 JavaScript object를 string으로 바꿔준다. 그리고 JSON.parse는 string을 JavaScript object로 바꿔준다.
+//         switch (message.type){
+//             case "new_message":
+//             //socket이 new_message type 메시지를 보내면 //parsed.type이 new_message와 같다면, 아래 코드를 실행시킨다.
+//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//                     //다른 모두에게 그 익명의 socket이 보낸 메시지를 전달할 것이다.
+//                     //메시지가 socket에서 전송되고 메시지의 type이 new_message이면 nickname property를 socket object에 저장하고 있다.
+//             case "nickname":
+//                     //이 payload 즉 nickname을 socket 안에 넣어줘야 한다. socket이 누군지 알고싶으니까.
+//                 socket["nickname"] = message.payload;
+//                     //여기서 우리가 새롭게 알게된 property는 바로 socket 안에 정보를 저장할 수 있다는 사실이다. 그냥 데이터를 주면된다. 넣고 싶은 걸 넣어주면 된다.
+//                     //socket에 새로운 item을 추가하자. socket은 기본적으로 객체니까.
+//                 console.log(message.payload);
+//                     //내가 nickname type message를 받으면, 받은 nickname을 socket에 넣어준다.
+//         }
+//                     //if else를 반복하는 것 대신, 테스트하고 싶은 것에 switch를 써주면 된다.
+//     });
+// });
+                    // if(parsed.type === "new_message"){
+                    // sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+                    // } else if(parsed.type === "nickname"){
+                    //     console.log(parsed.payload);
+                    // }
+
+// const handleListen = () => console.log(`Listening on http://localhost:3000`);
+//     //express는 http를 다루지만, 이젠 ws를 다룰거다. 여기에 2개를 합칠거다.
+//     //express로 같은 서버에 ws 기능을 설치할 거다.
+// server.listen(3000, handleListen);
+//     //이제 할 것은, server.listen 할 수 있다. port는 아무거나.  
+//     //이제 handListen을 사용할 수 있다.
